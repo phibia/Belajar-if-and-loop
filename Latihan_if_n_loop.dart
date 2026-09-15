e16f94471f19fae1983023c3818ab1b194e94b64
darly <se4eiw@gmail.com>
2026-09-15 19:54:10 +0700

    UPDATE -- LATIHAN PPT IF AND LOOP

--- Latihan_if_n_loop.dart
+++ Latihan_if_n_loop.dart
@@ -1,13 +1,46 @@
 void main() {
-  // Melakukan perulangan dari angka 1 sampai 10
-  for (int i = 1; i <= 10; i++) {
-    
-    // Memeriksa apakah angka genap atau ganjil
-    if (i % 2 == 0) {
-      print("Angka $i adalah Genap");
-    } else {
-      print("Angka $i adalah Ganjil");
-    }
-    
+
+  // for
+  List datamhs = ['Sindy', 'Windy', 'Karen', 'Vincent'];
+
+  for (var i = 0; i < datamhs.length; i++) {
+    print('nama mhs ke ${i + 1} adalah ${datamhs[i]}');
   }
+
+
+  // for in
+  List datamhs2 = ['Sindy', 'Windy', 'Karen', 'Vincent'];
+
+  for (var element in datamhs2) {
+    print('nama mhs adalah $element');
+  }
+
+
+  // foreach
+  List datamhs3 = ['Sindy', 'Windy', 'Karen', 'Vincent'];
+
+  datamhs3.forEach((a) {
+    print('nama mhs ke adalah $a');
+  });
+
+
+  // while
+  List datamhs4 = ['Sindy', 'Windy', 'Karen', 'Vincent'];
+  var i = 0;
+
+  while (i < datamhs4.length) {
+    print('nama mhs ke ${i + 1} adalah ${datamhs4[i]}');
+    i++;
+  }
+
+
+  // do while
+  List datamhs5 = ['Sindy', 'Windy', 'Karen', 'Vincent'];
+  var j = 0;
+
+  do {
+    print('nama mhs ke ${j + 1} adalah ${datamhs5[j]}');
+    j++;
+  } while (j < datamhs5.length);
+
 }
