1e1c7ae2bca09d6c2cb1212ae6cbd765d3645ce1
darly <se4eiw@gmail.com>
2026-09-15 19:08:12 +0700

    Update: Menambahkan codingan if dan loop Dart ke-1

---
+++ Latihan_if_n_loop.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Melakukan perulangan dari angka 1 sampai 10
+  for (int i = 1; i <= 10; i++) {
+    
+    // Memeriksa apakah angka genap atau ganjil
+    if (i % 2 == 0) {
+      print("Angka $i adalah Genap");
+    } else {
+      print("Angka $i adalah Ganjil");
+    }
+    
+  }
+}
